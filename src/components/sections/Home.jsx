6902b970ad313ec945042c454a4894793a1ea333
Manya Gupta <manyagupta09022001@gmail.com>
2025-10-21 23:42:21 +0530

    Update resume download button and minor fixes

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -21,8 +21,8 @@ export const Home = () => {
           </p>
           <div className="flex justify-center space-x-4">
             <a
-              href="#projects"
-              download=""
+              href="./ManyaGupta-Resume.pdf"
+              download
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
             >
               Download Resume
